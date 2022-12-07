dbacc5548b80826e980a20a1350de12247949d63
aliceakesson <alicek732@gmail.com>
2022-12-07 22:13:52 +0100

    Added template for day 8

---
+++ 2022/day8/solution.scala
@@ -0,0 +1,13 @@
+import scala.io.Source
+
+object Day8:
+    @main
+    def run: Unit =
+        part1
+        part2
+
+    def part1: Unit = 
+        val src = Source.fromFile("C:/Users/alcea/coding/aoc/2022/day7/puzzle.txt").getLines.toVector
+
+    def part2: Unit = println("")
+       
