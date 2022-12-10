190056a9583179d919af11b4247b05f3b657a9f7
aliceakesson <alicek732@gmail.com>
2022-12-10 09:32:15 +0100

    First version of day 10

--- 2022/day10/solution.scala
+++ 2022/day10/solution.scala
@@ -8,9 +8,28 @@ object Day10:
 
     def part1: Unit = 
         val src = Source.fromFile("C:/Users/alcea/coding/aoc/2022/day10/puzzle.txt").getLines.toVector
+        var x = 1
+        var cycle = 1
+        var sum = 0
+
+        val cycles = Vector(20, 60, 100, 140, 180, 220)
+
+        src.foreach( row =>  
+            if row.startsWith("addx") then 
+                if cycles.contains(cycle) then sum += x * cycle
+                else if cycles.contains(cycle+1) then sum += x * (cycle+1)
+                cycle += 2
+                try 
+                    x += row.substring(row.lastIndexOf(" ")).trim().toInt
+                catch
+                    case nfe: NumberFormatException => println()
+                if cycles.contains(cycle) then sum += x * cycle
+            else 
+                cycle += 1   
+                if cycles.contains(cycle) then sum += x * cycle
+        )
         
+        println(sum)
 
     def part2: Unit =
         val src = Source.fromFile("C:/Users/alcea/coding/aoc/2022/day10/puzzle.txt").getLines.toVector
-        
-       
