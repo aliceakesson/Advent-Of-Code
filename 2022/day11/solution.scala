f4d0721d3ae417168e55e1af39ef30814dda3803
aliceakesson <alicek732@gmail.com>
2023-12-03 13:37:02 +0100

    Added 2023 and day1

--- 2022/day11/solution.scala
+++ 2022/day11/solution.scala
@@ -13,37 +13,45 @@ object Day11:
         var inspections = Vector.fill(src.length)(0)
 
         for i <- 0 until 20 do 
+            println(i)
             src.foreach( monkey => 
-                val index = monkey(0).substring(monkey(0).lastIndexOf(" ")).trim()(0).toInt-48
-                val items = monkey(1).substring(monkey(1).indexOf(" ", monkey(1).indexOf(":"))).trim().split(", ").toVector
-                inspections = inspections.updated(index, inspections(index)+items.length)
+                if monkey(1).last != ' ' && monkey(1).last != ':' then
+                    val index = monkey(0)(monkey(0).length-2).toInt-48
+                    var items = Vector.empty[String]
+                    if monkey(1).length > "Starting items: ".length then 
+                        items = monkey(1).substring(monkey(1).indexOf(" ", monkey(1).indexOf(":"))).trim().split(", ").toVector
+                    inspections = inspections.updated(index, inspections(index)+items.length)
+                    if i < 2 then println(items) 
+                    items.foreach( item =>
+                        var worry = item.toInt
 
-                items.foreach( item =>
+                        var otherNumber = worry
+                        if monkey(2).substring(monkey(2).lastIndexOf(" ")).trim() != "old" then 
+                            try otherNumber = monkey(2).substring(monkey(2).lastIndexOf(" ")).trim().toInt
+                            catch case nfe: NumberFormatException => throw new NumberFormatException
 
-                    var worry = item.toInt
+                        if monkey(2).substring(monkey(2).indexOf(" ", monkey(2).indexOf("old"))).trim()(0) == '*' then
+                            worry *= otherNumber
+                        else 
+                            worry += otherNumber
 
-                    var otherNumber = worry
-                    if monkey(2).substring(monkey(2).lastIndexOf(" ")).trim() != "old" then 
-                        try otherNumber = monkey(2).substring(monkey(2).lastIndexOf(" ")).trim().toInt
-                        catch case nfe: NumberFormatException => throw new NumberFormatException
+                        worry = (worry / 3).toInt
 
-                    if monkey(2).substring(monkey(2).indexOf(" ", monkey(2).indexOf("old"))).trim()(0) == '*' then
-                        worry *= otherNumber
-                    else 
-                        worry += otherNumber
+                        val divisibleNumber = monkey(3).substring(monkey(3).lastIndexOf(" ")).trim().toInt
+                        var otherMonkey = 0
+                        if worry % divisibleNumber == 0 then 
+                            otherMonkey = monkey(4).substring(monkey(4).lastIndexOf(" ")).trim().toInt
+                        else 
+                            otherMonkey = monkey(5).substring(monkey(5).lastIndexOf(" ")).trim().toInt
 
-                    worry = (worry / 3).toInt
+                        val text1 = src(otherMonkey)(1).concat(s", $worry")
+                        var text2 = "Starting items: "
+                        if items.length > 1 then 
+                            text2 = monkey(1).substring(monkey(1).indexOf(",")+1).trim() 
 
-                    val divisibleNumber = monkey(3).substring(monkey(3).lastIndexOf(" ")).trim().toInt
-                    var otherMonkey = 0
-                    if worry % divisibleNumber == 0 then 
-                        otherMonkey = monkey(4).substring(monkey(4).lastIndexOf(" ")).trim().toInt
-                    else 
-                        otherMonkey = monkey(5).substring(monkey(5).lastIndexOf(" ")).trim().toInt
-
-                    val text = src(otherMonkey)(0).concat(s", $worry")
-                    src = src.updated(otherMonkey, src(otherMonkey).updated(0, text))
-                )
+                        src = src.updated(otherMonkey, src(otherMonkey).updated(1, text1))
+                        src = src.updated(index, src(index).updated(1, text2))
+                    )
             )
 
         println(inspections)
---
+++ 2023/day1/solution.scala
@@ -0,0 +1,86 @@
+import scala.io.Source
+
+object Day11:
+    @main
+    def run: Unit =
+        part1
+        part2
+
+    def part1: Unit = 
+        var src = Source.fromFile("C:/Users/alcea/coding/aoc/2023/day1/puzzle.txt").getLines.toVector
+
+        var sum = 0
+
+        src.foreach(line => 
+            var found = false
+            var a = '0'
+            var b = '0'
+
+            line.foreach(c =>
+                if c.isDigit && !found then 
+                    a = c
+                    found = true
+            )
+
+            found = false
+            line.reverse.foreach(c =>
+                if c.isDigit && !found then 
+                    b = c
+                    found = true
+            )
+
+            val num = a.toString() + b.toString() + ""
+            val numToInt = num.toInt
+
+            sum += numToInt
+            println(s"a: $a, b: $b, num: $num, numtoInt: $numToInt")
+        )
+
+        println(sum)
+
+    def part2: Unit =
+        var src = Source.fromFile("C:/Users/alcea/coding/aoc/2023/day1/puzzle.txt").getLines.toVector
+
+        var sum = 0
+        val words = Map("one" -> '1', "two" -> '2', "three" -> '3', "four" -> '4', "five" -> '5', "six" -> '6', "seven" -> '7', "eight" -> '8', "nine" -> '9')
+
+        src.foreach(line => 
+            var found = false
+            var a = '0'
+            var b = '0'
+
+            var a_index = -1
+            var b_index = -1
+
+            line.foreach(c =>
+                if c.isDigit && !found then 
+                    a = c
+                    a_index = line.indexOf(c)
+                    found = true
+            )
+
+            found = false
+            line.reverse.foreach(c =>
+                if c.isDigit && !found then 
+                    b = c
+                    b_index = line.lastIndexOf(c)
+                    found = true
+            )
+
+            words.keySet.foreach(word =>
+                if line.indexOf(word) >= 0 && (a_index < 0 || a_index > line.indexOf(word)) then
+                     a  = words.get(word).get
+                     a_index = line.indexOf(word)
+                if line.lastIndexOf(word) >= 0 && (b_index < 0 || b_index < line.lastIndexOf(word)) then
+                     b  = words.get(word).get
+                     b_index = line.lastIndexOf(word)
+            )
+
+            val num = a.toString() + b.toString() + ""
+            val numToInt = num.toInt
+
+            sum += numToInt
+            println(s"a: $a, b: $b, num: $num, numtoInt: $numToInt")
+        )
+
+        println(sum)
