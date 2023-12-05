244be2da3ac9f6f5dcf04c07190190e34c7f0acc
aliceakesson <alicek732@gmail.com>
2023-12-05 10:35:05 +0100

    Added folder for day6

---
+++ 2023/day6/solution.scala
@@ -0,0 +1,22 @@
+import scala.io.Source
+
+object Day6:
+    @main
+    def run: Unit =
+        part1
+        part2
+
+    def part1: Unit = 
+        var src = Source.fromFile("C:/Users/alcea/coding/aoc/2023/day6/puzzle.txt").getLines.toVector
+
+
+    def part2: Unit =
+        var src = Source.fromFile("C:/Users/alcea/coding/aoc/2023/day6/puzzle.txt").getLines.toVector
+
+    
+
+
+
+       
+
+        
