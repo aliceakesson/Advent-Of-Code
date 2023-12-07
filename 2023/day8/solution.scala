09c8dcf0f8e94c6545fe9d07559213a8a96f305c
aliceakesson <alicek732@gmail.com>
2023-12-07 11:46:15 +0100

    Added folder for day8

---
+++ 2023/day8/solution.scala
@@ -0,0 +1,16 @@
+import scala.io.Source
+
+object Day8:
+    @main
+    def run: Unit =
+        part1
+        part2
+
+    def part1: Unit = 
+        var src = Source.fromFile("C:/Users/alcea/coding/aoc/2023/day8/puzzle.txt").getLines.toVector
+
+
+    def part2: Unit =
+        var src = Source.fromFile("C:/Users/alcea/coding/aoc/2023/day8/puzzle.txt").getLines.toVector
+
+
