38ff8b67e66546536c3889530b16ba77a7c88307
aliceakesson <alicek732@gmail.com>
2023-12-09 10:25:03 +0100

    Added folder for day10

---
+++ 2023/day10/solution.scala
@@ -0,0 +1,16 @@
+import scala.io.Source
+val pathToFolder = "C:/Users/alcea/coding/aoc/"
+
+object Day10:
+    @main
+    def run: Unit =
+        part1
+        part2
+
+    def part1: Unit = 
+        var src = Source.fromFile(pathToFolder + "/2023/day10/puzzle.txt").getLines.toVector
+
+
+    def part2: Unit =
+        var src = Source.fromFile(pathToFolder + "/2023/day10/puzzle.txt").getLines.toVector
+
