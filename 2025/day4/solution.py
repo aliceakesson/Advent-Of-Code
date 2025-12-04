0ffcc0cc0c6fe861d5ff2946c837f25d144b021a
Alice Åkesson <alicek732@gmail.com>
2025-12-04 22:01:29 +0100

    day4 done

--- 2025/day4/solution.py
+++ 2025/day4/solution.py
@@ -1,7 +1,51 @@
 def part1():
     f = open("puzzle.txt", "r")
+    sum = 0
+    lines = []
+    for line in f: 
+        lines.append(line)
+
+    for row, line in enumerate(lines): 
+        line = line.replace('\n', '')
+        for col, roll in enumerate(line): 
+            if roll == '@':
+                adjacent = 0
+                for i in range(-1, 2):
+                    r = row + i
+                    for j in range(-1, 2):
+                        c = col + j
+                        if not (r < 0 or c < 0 or r > len(lines)-1 or c > len(line)-1):
+                            if not (i == 0 and j == 0) and lines[r][c] == '@': adjacent += 1
+                if adjacent < 4: 
+                    sum += 1
+    print(sum)
 def part2():
     f = open("puzzle.txt", "r")
+    sum = 0
+    lines = []
+    for line in f: 
+        lines.append(line)
+
+    while True:
+        found = False
+        for row, line in enumerate(lines): 
+            line = line.replace('\n', '')
+            for col, roll in enumerate(line): 
+                if roll == '@':
+                    adjacent = 0
+                    for i in range(-1, 2):
+                        r = row + i
+                        for j in range(-1, 2):
+                            c = col + j
+                            if not (r < 0 or c < 0 or r > len(lines)-1 or c > len(line)-1):
+                                if not (i == 0 and j == 0) and lines[r][c] == '@': adjacent += 1
+                    if adjacent < 4: 
+                        found = True
+                        sum += 1
+                        lines[row] = lines[row][:col] + '.' + lines[row][col+1:]
+        if found: continue
+        else: break 
+    print(sum)
     
 part1()
 part2()
